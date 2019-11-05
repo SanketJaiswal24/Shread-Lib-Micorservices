b35fddedfd684682a227967fd6566e2cf59070f1
sanket.jaiswal <sanket.jaiswal2409@gmail.com>
2019-11-05 15:30:01 +0530

    jacaco added

---
+++ vars/jacacoReport.groovy
@@ -0,0 +1,12 @@
+def call()
+{
+    stage("Publish Jacoca Report"){
+       /*Publish Jacoca Report in Jenkins Dashboard */
+            junit 'target/**/*.xml'
+            step([
+              $class           : 'JacocoPublisher',
+              execPattern      : 'target/jacoco.exec',
+              sourcePattern    : '**/src/main/java'
+           ])
+   }
+}
